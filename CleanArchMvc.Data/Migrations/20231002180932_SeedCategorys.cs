cf08ed0835f3ffb960d7c87756be37285b2fb721
Andressa Atelli <andressaatelli.programador@gmail.com>
2023-10-02 16:20:58 -0300

    Populando banco pela migration

---
+++ CleanArchMvc.Data/Migrations/20231002180932_SeedCategorys.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategorys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories(Name)" + "VALUES('Material Escolar')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name)" + "VALUES('Material Informatica')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name)" + "VALUES('Material Escritorio')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categories");
+        }
+    }
+}
---
+++ CleanArchMvc.Data/Migrations/20231002191536_SeedProducts.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" + "VALUES('Caderno espiral', 'Caderno espiral 100 fls', 7.45, 50, 'Caderno1.jpg', 1)");
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" + "VALUES('Mouse', 'Mouse sem fio', 35.00, 15, 'MouseSemFio.jpg', 2)");
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" + "VALUES('Caderno espiral', 'Caderno espiral 200 fls', 9.45, 50, 'Caderno3.jpg', 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
