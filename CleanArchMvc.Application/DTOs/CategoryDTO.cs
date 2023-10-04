bc75435c3ca48b629f905344545cb965ae0a8206
Andressa Atelli <andressaatelli.programador@gmail.com>
2023-10-04 19:36:23 -0300

    Inserção do autoMapper

---
+++ CleanArchMvc.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The name is Required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+    }
+}
---
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,45 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The name is Required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
+        public string Name { get; protected set; }
+
+        [Required(ErrorMessage = "The Description is Required")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        [DisplayName("Description")]
+        public string Description { get; private set; }
+
+        [Required(ErrorMessage = "The Price is Required")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Price")]
+        public decimal Price { get; private set; }
+
+        [Required(ErrorMessage = "The Stock is Required")]
+        [Range(1, 9999)]
+        [DisplayName("Stock")]
+        public int Stock { get; private set; }
+
+        [MaxLength(50)]
+        [DisplayName("Product Image")]
+        public string Image { get; private set; }
+
+        public Category Category { get; private set; }
+
+        [DisplayName("Categories")]
+        public int CategoryId { get; private set; }
+    }
+}
